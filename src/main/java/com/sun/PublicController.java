2175965ff25f3a760ce004ee0dbe28eac9738434
孙少伟 <136107229@qq.com>
2017-11-10 17:35:38 +0800

    fix

--- src/main/java/com/sun/PublicController.java
+++ src/main/java/com/sun/PublicController.java
@@ -1,6 +1,7 @@
 package com.sun;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
